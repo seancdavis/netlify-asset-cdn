05a7f1c8fe48a331faaed8066964a9d9358093e8
Sean C Davis <scdavis41@gmail.com>
2025-06-28 08:05:51 -0400

    Setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: netlify(),
+  integrations: [react()],
 });
